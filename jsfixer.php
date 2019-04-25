23617f8bfafee9fdc6fad025eb3896fef0f2a278
Arlen P Walker <arlen.walker@pkware.com>
2019-04-25 14:41:19 -0500

    Handles replacing two core hacks
    In both popover and tooltip, calls were made without checking to see if the object existed they were being called upon. Added a test to avoid throwing 'unsupported function' javascript errors.

---
+++ jsfixer.php
@@ -0,0 +1,197 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die;
+
+class PlgSystemJsfixer extends JPlugin
+{
+	/**
+	 * Application object
+	 *
+	 * @var  JApplicationCms
+	 */
+	protected $app;
+	/**
+	 * Array containing information for loaded files
+	 *
+	 * @var  array
+	 */
+	protected static $loaded = array();
+	/**
+	 * Listener for onAfterInitialise event
+	 *
+	 * @return  void
+	 */
+	public function onAfterInitialise()
+	{
+		// Only for site
+		if (!$this->app->isSite())
+		{
+			return;
+		}
+		// Register listeners for JHtml helpers
+		if (!JHtml::isRegistered('bootstrap.popover'))
+		{
+			JHtml::register('bootstrap.popover', 'PlgSystemJsfixer::popover');
+		}
+
+		if (!JHtml::isRegistered('bootstrap.tooltip'))
+		{
+			JHtml::register('bootstrap.tooltip', 'PlgSystemJsfixer::tooltip');
+		}
+
+	}
+	/**
+	 * Override javascript support for Bootstrap popovers with code that checks to
+	 * see if it's needed before trying to add itself, thus avoiding throwing a js error.
+	 *
+	 * Use element's Title as popover content
+	 *
+	 * @param   string  $selector  Selector for the popover
+	 * @param   array   $params    An array of options for the popover.
+	 *                  Options for the popover can be:
+	 *                      animation  boolean          apply a css fade transition to the popover
+	 *                      html       boolean          Insert HTML into the popover. If false, jQuery's text method will be used to insert
+	 *                                                  content into the dom.
+	 *                      placement  string|function  how to position the popover - top | bottom | left | right
+	 *                      selector   string           If a selector is provided, popover objects will be delegated to the specified targets.
+	 *                      trigger    string           how popover is triggered - hover | focus | manual
+	 *                      title      string|function  default title value if `title` tag isn't present
+	 *                      content    string|function  default content value if `data-content` attribute isn't present
+	 *                      delay      number|object    delay showing and hiding the popover (ms) - does not apply to manual trigger type
+	 *                                                  If a number is supplied, delay is applied to both hide/show
+	 *                                                  Object structure is: delay: { show: 500, hide: 100 }
+	 *                      container  string|boolean   Appends the popover to a specific element: { container: 'body' }
+	 *
+	 * @return  void
+	 *
+	 * @since   3.0
+	 */
+	public static function popover($selector = '.hasPopover', $params = array())
+	{
+		// Only load once
+		if (isset(static::$loaded[__METHOD__][$selector]))
+		{
+			return;
+		}
+
+		// Include Bootstrap framework
+		JHtml::_('bootstrap.framework');
+
+		$opt['animation'] = isset($params['animation']) ? $params['animation'] : null;
+		$opt['html']      = isset($params['html']) ? $params['html'] : true;
+		$opt['placement'] = isset($params['placement']) ? $params['placement'] : null;
+		$opt['selector']  = isset($params['selector']) ? $params['selector'] : null;
+		$opt['title']     = isset($params['title']) ? $params['title'] : null;
+		$opt['trigger']   = isset($params['trigger']) ? $params['trigger'] : 'hover focus';
+		$opt['content']   = isset($params['content']) ? $params['content'] : null;
+		$opt['delay']     = isset($params['delay']) ? $params['delay'] : null;
+		$opt['container'] = isset($params['container']) ? $params['container'] : 'body';
+
+		$options = JHtml::getJSObject($opt);
+
+		$initFunction = 'function initPopovers (event, container) { ' .
+				'if ($(container || document).find(' . json_encode($selector) . ').popover) {' .
+					'$(container || document).find(' . json_encode($selector) . ').popover(' . $options . ');' .
+				'}' .
+			'}';
+
+		// Attach the popover to the document
+		JFactory::getDocument()->addScriptDeclaration(
+			'jQuery(function($){ initPopovers(); $("body").on("subform-row-add", initPopovers); ' . $initFunction . ' });'
+		);
+
+		static::$loaded[__METHOD__][$selector] = true;
+
+		return;
+	}
+	/**
+	 * Add javascript support for Bootstrap tooltips
+	 *
+	 * Add a title attribute to any element in the form
+	 * title="title::text"
+	 *
+	 * @param   string  $selector  The ID selector for the tooltip.
+	 * @param   array   $params    An array of options for the tooltip.
+	 *                             Options for the tooltip can be:
+	 *                             - animation  boolean          Apply a CSS fade transition to the tooltip
+	 *                             - html       boolean          Insert HTML into the tooltip. If false, jQuery's text method will be used to insert
+	 *                                                           content into the dom.
+	 *                             - placement  string|function  How to position the tooltip - top | bottom | left | right
+	 *                             - selector   string           If a selector is provided, tooltip objects will be delegated to the specified targets.
+	 *                             - title      string|function  Default title value if `title` tag isn't present
+	 *                             - trigger    string           How tooltip is triggered - hover | focus | manual
+	 *                             - delay      integer          Delay showing and hiding the tooltip (ms) - does not apply to manual trigger type
+	 *                                                           If a number is supplied, delay is applied to both hide/show
+	 *                                                           Object structure is: delay: { show: 500, hide: 100 }
+	 *                             - container  string|boolean   Appends the popover to a specific element: { container: 'body' }
+	 *
+	 * @return  void
+	 *
+	 * @since   3.0
+	 */
+	public static function tooltip($selector = '.hasTooltip', $params = array())
+	{
+		if (!isset(static::$loaded[__METHOD__][$selector]))
+		{
+			// Include Bootstrap framework
+			JHtml::_('bootstrap.framework');
+
+			// Setup options object
+			$opt['animation'] = isset($params['animation']) ? (boolean) $params['animation'] : null;
+			$opt['html']      = isset($params['html']) ? (boolean) $params['html'] : true;
+			$opt['placement'] = isset($params['placement']) ? (string) $params['placement'] : null;
+			$opt['selector']  = isset($params['selector']) ? (string) $params['selector'] : null;
+			$opt['title']     = isset($params['title']) ? (string) $params['title'] : null;
+			$opt['trigger']   = isset($params['trigger']) ? (string) $params['trigger'] : null;
+			$opt['delay']     = isset($params['delay']) ? (is_array($params['delay']) ? $params['delay'] : (int) $params['delay']) : null;
+			$opt['container'] = isset($params['container']) ? $params['container'] : 'body';
+			$opt['template']  = isset($params['template']) ? (string) $params['template'] : null;
+			$onShow           = isset($params['onShow']) ? (string) $params['onShow'] : null;
+			$onShown          = isset($params['onShown']) ? (string) $params['onShown'] : null;
+			$onHide           = isset($params['onHide']) ? (string) $params['onHide'] : null;
+			$onHidden         = isset($params['onHidden']) ? (string) $params['onHidden'] : null;
+
+			$options = JHtml::getJSObject($opt);
+
+			// Build the script.
+			$conditional = 'if ($(' . json_encode($selector) . ').tooltip) { ';
+			$script = array('$(container).find(' . json_encode($selector) . ').tooltip(' . $options . ')');
+
+			if ($onShow)
+			{
+				$script[] = 'on("show.bs.tooltip", ' . $onShow . ')';
+			}
+
+			if ($onShown)
+			{
+				$script[] = 'on("shown.bs.tooltip", ' . $onShown . ')';
+			}
+
+			if ($onHide)
+			{
+				$script[] = 'on("hide.bs.tooltip", ' . $onHide . ')';
+			}
+
+			if ($onHidden)
+			{
+				$script[] = 'on("hidden.bs.tooltip", ' . $onHidden . ')';
+			}
+
+			$initFunction = 'function initTooltips (event, container) { ' .
+				'container = container || document;' .
+				$conditional .
+				implode('.', $script) . ';' .
+				'}}';
+
+			// Attach tooltips to document
+			JFactory::getDocument()
+				->addScriptDeclaration('jQuery(function($){ initTooltips(); $("body").on("subform-row-add", initTooltips); ' . $initFunction . ' });');
+
+			// Set static array
+			static::$loaded[__METHOD__][$selector] = true;
+		}
+
+		return;
+	}
+
+}
